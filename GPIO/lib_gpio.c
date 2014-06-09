19e3f2e4b2b093b613f8e53b6b750b1560ba5ba4
Michael Zangerle <michael.zangerle@gmail.com>
2014-06-09 09:59:46 +0200

    implemented led

---
+++ GPIO/lib_gpio.c
@@ -0,0 +1,15 @@
+/*
+ * Library: lib_gpio.c
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: 09.06.2014
+ * Description: 
+ * TODO
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "lib_gpio.h"
+#include "../Syscall/lib_syscall.h"
+
+
---
+++ GPIO/lib_gpio.h
@@ -0,0 +1,14 @@
+/*
+ * Library: lib_gpio.h
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: 09.06.2014
+ * Description: 
+ * TODO
+ */
+
+#ifndef LIB_GPIO_H_
+#define LIB_GPIO_H_
+
+
+#endif /* LIB_GPIO_H_ */
