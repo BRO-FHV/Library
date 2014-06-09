d03cb0148a78f3d532883e61867f18d33502e9a6
thomasgaida <thomas.gaida@students.fhv.at>
2014-06-09 09:34:37 +0200

    added defines for led und gpio swi

--- Syscall/lib_syscall.h
+++ Syscall/lib_syscall.h
@@ -13,7 +13,20 @@
 #define LIB_SYSCALL_H_
 
 #define SYSCALL_PRINTF 0
-#define SYSCALL_PRINTF_TEST 1
+#define SYSCALL_LED_ON_0 1
+#define SYSCALL_LED_ON_1 2
+#define SYSCALL_LED_ON_2 3
+#define SYSCALL_LED_ON_3 4
+#define SYSCALL_LED_OFF_0 5
+#define SYSCALL_LED_OFF_1 6
+#define SYSCALL_LED_OFF_2 7
+#define SYSCALL_LED_OFF_3 8
+#define SYSCALL_GPIO_DIR_SET 9
+#define SYSCALL_GPIO_WRITE 10
+
+
+
+#define SYSCALL_PRINTF_TEST 100
 
 typedef struct SyscallArg
 {
