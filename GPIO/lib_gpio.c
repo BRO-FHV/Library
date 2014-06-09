2f608f51ec98308693daa73f1b666daece09d7e4
Michael Zangerle <michael.zangerle@gmail.com>
2014-06-09 11:20:52 +0200

    added gpio

--- GPIO/lib_gpio.c
+++ GPIO/lib_gpio.c
@@ -13,3 +13,28 @@
 #include "../Syscall/lib_syscall.h"
 
 
+void lib_gpio_dir_mode_set(unsigned int baseAdd,unsigned int pinNumber,unsigned int pinDirection){
+	SyscallArgData data;
+	data.swiNumber = SYSCALL_GPIO_DIR_SET;
+	data.arg1 = baseAdd;
+	data.arg2 = pinNumber;
+	data.arg3 = pinDirection;
+	Syscall(&data);
+}
+
+void lib_gpio_pin_write(unsigned int baseAdd, unsigned int pinNumber,unsigned int pinValue){
+	SyscallArgData data;
+	data.swiNumber = SYSCALL_GPIO_PIN_WRITE;
+	data.arg1 = baseAdd;
+	data.arg2 = pinNumber;
+	data.arg3 = pinValue;
+	Syscall(&data);
+}
+
+unsigned int lib_gpio_pin_read(unsigned int baseAdd, unsigned int pinNumber){
+	SyscallArgData data;
+	data.swiNumber = SYSCALL_GPIO_PIN_READ;
+	data.arg1 = baseAdd;
+	data.arg2 = pinNumber;
+	Syscall(&data);
+}
--- GPIO/lib_gpio.h
+++ GPIO/lib_gpio.h
@@ -10,5 +10,19 @@
 #ifndef LIB_GPIO_H_
 #define LIB_GPIO_H_
 
+/**
+ * Set a gpio pin
+ */
+void lib_gpio_dir_mode_set(unsigned int baseAdd,unsigned int pinNumber,unsigned int pinDirection);
+
+/**
+ * Write on a gpio pin
+ */
+void lib_gpio_pin_write(unsigned int baseAdd, unsigned int pinNumber,unsigned int pinValue);
+
+/**
+ * Read from a gpio pin
+ */
+unsigned int lib_gpio_pin_read(unsigned int baseAdd, unsigned int pinNumber);
 
 #endif /* LIB_GPIO_H_ */
