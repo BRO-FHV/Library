d9ab9e012ff909bfca71aba07abe243e966be06e
sla89 <skullsplitter@die-optimisten.net>
2014-06-14 17:00:47 +0200

    added ethernet library files - need to implementet them sometime... @thomasgaida

---
+++ Eth/lib_eth.c
@@ -0,0 +1,10 @@
+/*
+ * Library: lib_eth.c
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: 14.06.2014
+ * Description: 
+ * TODO
+ */
+
+
---
+++ Eth/lib_eth.h
@@ -0,0 +1,15 @@
+/*
+ * Library: lib_eth.h
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: 14.06.2014
+ * Description: 
+ * TODO
+ */
+
+#ifndef LIB_ETH_H_
+#define LIB_ETH_H_
+
+
+
+#endif /* LIB_ETH_H_ */
