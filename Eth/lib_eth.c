6c4c67e3a943a51f5cc44ed8aaf9b3242d3e12f2
thomasgaida <thomas.gaida@students.fhv.at>
2014-06-18 11:41:13 +0200

    adde access method eth

--- Eth/lib_eth.c
+++ Eth/lib_eth.c
@@ -7,4 +7,44 @@
  * TODO
  */
 
+#include <inttypes.h>
+#include  <basic.h>
+#include "../Syscall/lib_syscall.h"
+#include <eth/dr_eth_udp.h>
+
+void SwiUdpInit(uint32_t port) {
+	SyscallArgData data;
+	data.swiNumber = SYSCALL_UDP_INIT;
+	data.arg1 = port;
+	Syscall(&data);
+}
+upd_package_t* SwiUdpGetData(uint32_t port) {
+	SyscallArgData data;
+	data.swiNumber = SYSCALL_UDP_GETDATA;
+	data.arg1 = port;
+	Syscall(&data);
+
+	return (upd_package_t*) data.result;
+}
+void SwiUdpSendData(uint8_t receiver[], uint32_t port, uint8_t* data,
+		uint32_t datalen) {
+
+	SyscallArgData dataarg;
+	dataarg.swiNumber = SYSCALL_UDP_SENDDATA;
+	dataarg.arg1 = port;
+	dataarg.arg2 = (uint32_t)receiver;
+	dataarg.arg3 = (uint32_t)data;
+	dataarg.arg4 = datalen;
+	Syscall(&dataarg);
+}
+tBoolean SwiUdpHasData(uint32_t port) {
+
+	SyscallArgData data;
+		data.swiNumber = SYSCALL_UDP_HASDATA;
+		data.arg1 = port;
+		Syscall(&data);
+
+		return (tBoolean) data.result;
+
+}
 
--- Eth/lib_eth.h
+++ Eth/lib_eth.h
@@ -10,6 +10,10 @@
 #ifndef LIB_ETH_H_
 #define LIB_ETH_H_
 
+void SwiUdpInit(uint32_t port);
+upd_package_t* SwiUdpGetData(uint32_t port);
+void SwiUdpSendData(uint8_t receiver[],uint32_t port,uint8_t* data, uint32_t datalen);
+tBoolean SwiUdpHasData(uint32_t port);
 
 
 #endif /* LIB_ETH_H_ */
